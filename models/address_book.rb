1bcb82b08f68561b74143e53566e346694e2a21b
Austin <admin@designxdevelop.com>
2015-09-30 11:21:11 -0700

    CP 21 Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,7 +1,20 @@
+require_relative 'entry'
 class AddressBook
    attr_accessor :entries
 
    def initialize
      @entries = []
+   end
+
+# => Can you please explain what the whole add_entry method is really doing
+   def add_entry(name,phone_number,email)
+     index = 0
+     @entries.each do |entry|
+       if name < entry.name
+         break
+       end
+       index += 1
+     end
+     @entries.insert(index, Entry.new(name, phone_number, email))
    end
  end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -23,5 +23,14 @@ RSpec.describe AddressBook do
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(book.entries.size).to eq(1)
     end
+    it "adds the correct information to entries" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      new_entry = book.entries[0]
+
+      expect(new_entry.name).to eq('Ada Lovelace')
+      expect(new_entry.phone_number).to eq('010.012.1815')
+      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
   end
 end
